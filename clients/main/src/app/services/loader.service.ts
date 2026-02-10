b2927e3958eda84dde2f70e2a497375192ef5bbc
smit patel <smit.dpatel9924@gmail.com>
2026-02-10 10:20:08 +0000

    feat: implement global preloader

---
+++ clients/main/src/app/services/loader.service.ts
@@ -0,0 +1,19 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoaderService {
+  private loaderSubject = new BehaviorSubject<boolean>(false);
+
+  loading$ = this.loaderSubject.asObservable();
+
+  showLoader() {
+    this.loaderSubject.next(true);
+  }
+
+  hideLoader() {
+    this.loaderSubject.next(false);
+  }
+}
