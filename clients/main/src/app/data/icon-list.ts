b76ecf2de7ae7ca1a3fb0cde9d5664c8661eb5b8
smit patel <smit.dpatel9924@gmail.com>
2026-02-16 04:15:49 +0000

    feat: integrate SVG icon support using Angular Material

---
+++ clients/main/src/app/data/icon-list.ts
@@ -0,0 +1,34 @@
+export const IconList = [
+    {
+        name: 'visibility',
+        src: 'assets/icons/visibility.svg'
+    },
+    {
+        name: 'visibilityOff',
+        src: 'assets/icons/visibility-off.svg'
+    },
+    {
+        name: 'person',
+        src: 'assets/icons/person.svg'
+    },
+    {
+        name: 'lock',
+        src: 'assets/icons/lock.svg'
+    },
+    {
+        name: 'googleLogo',
+        src: 'assets/icons/google-logo.svg'
+    },
+    {
+        name: 'facebookLogoRounded',
+        src: 'assets/icons/facebook-logo-rounded.svg'
+    },
+    {
+        name: 'facebookLogo',
+        src: 'assets/icons/facebook-logo.svg'
+    },
+    {
+        name: 'xTwitterLogo',
+        src: 'assets/icons/x-twitter-logo.svg'
+    }
+]
