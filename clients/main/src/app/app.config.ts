d59200f3d608e2d932f2b7fa34136832ce620364
smit patel <smit.dpatel9924@gmail.com>
2026-02-17 05:46:12 +0000

    feat: set outline as default appearance for mat-form-field inputs

--- clients/main/src/app/app.config.ts
+++ clients/main/src/app/app.config.ts
@@ -2,10 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        appearance: 'outline',
+        floatLabel: 'always',
+      }
+    },
   ]
 };
