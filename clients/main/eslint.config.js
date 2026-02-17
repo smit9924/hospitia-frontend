582448a2d8c0c49c5312382f31839a9903e398e0
smit patel <smit.dpatel9924@gmail.com>
2026-02-17 05:38:46 +0000

    chore: integrate Prettier with ESLint and add formatting configuration

--- clients/main/eslint.config.js
+++ clients/main/eslint.config.js
@@ -3,6 +3,7 @@ const eslint = require("@eslint/js");
 const { defineConfig } = require("eslint/config");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   {
@@ -12,6 +13,7 @@ module.exports = defineConfig([
       tseslint.configs.recommended,
       tseslint.configs.stylistic,
       angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
