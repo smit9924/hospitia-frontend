d45c0142a1be68f688ae0ab76c6caf9eac1089fe
smit patel <smit.dpatel9924@gmail.com>
2026-02-17 05:49:44 +0000

    feat: implement responsive login form design

--- clients/main/src/app/app.routes.ts
+++ clients/main/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { StyleGuide } from './components/style-guide/style-guide/style-guide';
+import { Login } from './components/auth/login/login';
 
 export const routes: Routes = [
     {
         path: "style-guide",
         component: StyleGuide,
+    },
+    {
+        path: 'login',
+        component: Login,
     }
 ];
