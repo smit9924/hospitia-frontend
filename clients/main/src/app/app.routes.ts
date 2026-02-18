2b97f47cd31e5bad46e568c51008af21397ba80e
smit patel <smit.dpatel9924@gmail.com>
2026-02-18 04:35:08 +0000

    feat(auth): create signup component and configure routing

--- clients/main/src/app/app.routes.ts
+++ clients/main/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { StyleGuide } from './components/style-guide/style-guide/style-guide';
 import { Login } from './components/auth/login/login';
+import { Signup } from './components/auth/signup/signup';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'login',
     component: Login,
+  },
+  {
+    path: 'signup',
+    component: Signup,
   },
 ];
