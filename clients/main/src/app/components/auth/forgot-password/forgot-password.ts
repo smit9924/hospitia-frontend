538afc748a10c62cd3e255d346934f51d6a93127
Maitri Modi <maitri.modi@vayana.com>
2026-02-23 15:09:35 +0000

    created static forgot password screen

--- clients/main/src/app/components/auth/forgot-password/forgot-password.ts
+++ clients/main/src/app/components/auth/forgot-password/forgot-password.ts
@@ -1,9 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { APP_ROUTES } from '../../../data/app-routes';
+
+
+/* Angular Material */
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+  ],
   templateUrl: './forgot-password.html',
-  styleUrl: './forgot-password.scss',
+  styleUrls: ['./forgot-password.scss'],
 })
-export class ForgotPassword {}
+
+export class ForgotPassword {
+  APP_ROUTES = APP_ROUTES;
+  email: string = '';
+  loading = false;
+
+  onSubmit(): void {
+    if (!this.email || this.loading) return;
+
+    this.loading = true;
+
+    // 👉 Call your API here
+    console.log('Reset password requested for:', this.email);
+
+    // simulate API
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
+  }
+}
