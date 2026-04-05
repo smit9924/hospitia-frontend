0562910db22146dd9d06ce593b7cb6bacf9cabb2
smit patel <smit.dpatel9924@gmail.com>
2026-04-05 05:39:03 +0000

    chore(eslint): configure eslint to allow unused variable and arguments name starts with _ (underscore):

--- clients/main/eslint.config.js
+++ clients/main/eslint.config.js
@@ -33,6 +33,13 @@ module.exports = defineConfig([
           style: "kebab-case",
         },
       ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+        }
+      ],
     },
   },
   {
