dc070d35c4b6e6c5963bc4d462607b2ef1d7e953
smit patel <smit.dpatel9924@gmail.com>
2026-04-25 12:45:18 +0000

    feat(http-config-interceptor): add interceptor for generic HTTP request configuration (e.g., timeout handling)

---
+++ clients/main/src/app/interceptors/http-config/http-config-interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpContextToken, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError, timeout, TimeoutError } from 'rxjs';
+import { HTTP_REQ_TIMEOUT } from '../../types/enums/common';
+
+export const REQUEST_TIMEOUT = new HttpContextToken<HTTP_REQ_TIMEOUT>(
+  () => HTTP_REQ_TIMEOUT.DISABLED,
+);
+
+export const httpConfigInterceptor: HttpInterceptorFn = (req, next) => {
+  const timeoutValue = req.context.get(REQUEST_TIMEOUT);
+
+  // Skip timeout if disabled
+  if (timeoutValue === HTTP_REQ_TIMEOUT.DISABLED) {
+    return next(req);
+  }
+
+  // RxJS `timeout` operator automatically throws a `TimeoutError` if the request exceeds the specified time.
+  // If timeout is not disabled/handled explicitly, this error will propagate through the stream.
+  // The thrown `TimeoutError` is then caught by the global error handler for centralized handling.
+  return next(req).pipe(
+    timeout(timeoutValue),
+    catchError((error) => {
+      return throwError(() => error);
+    }),
+  );
+};
