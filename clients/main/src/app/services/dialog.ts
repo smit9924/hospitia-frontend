cc80c54f464b6e6efe2fb40166992ba75b0aa144
smit patel <smit.dpatel9924@gmail.com>
2026-04-25 12:18:57 +0000

    feat(dialog): add openOkDialog method for simplified dialog creation with Ok button

--- clients/main/src/app/services/dialog.ts
+++ clients/main/src/app/services/dialog.ts
@@ -2,6 +2,9 @@ import { inject, Injectable } from '@angular/core';
 import { GenericDialogConfig } from '../types/models/common/generic-dialog/generic-dialog-config';
 import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 import { GenericDialog } from '../components/common/generic-dialog/generic-dialog';
+import { GenericDialogButtonMetadata } from '../types/interfaces/common';
+import { GenericDialogButtonType } from '../types/enums/common';
+import { GenericDialogConfigData } from '../types/models/common/generic-dialog/generic-dialog-config-data';
 
 @Injectable({
   providedIn: 'root',
@@ -20,5 +23,27 @@ export class Dialog {
     );
 
     return ref;
+  }
+
+  openOkDialog(title: string, contentText: string): void {
+    const buttons: GenericDialogButtonMetadata[] = [
+      {
+        label: $localize`Ok`,
+        closeOnClick: true,
+        type: GenericDialogButtonType.FILLED,
+      },
+    ];
+
+    const dialogData = new GenericDialogConfigData({
+      buttons,
+      title,
+      contentText,
+    });
+
+    const dialogConfig = new GenericDialogConfig<GenericDialogConfigData>({
+      data: dialogData,
+    });
+
+    this.open(dialogConfig);
   }
 }
