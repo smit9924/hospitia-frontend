ee23bd67cdc0c244eca4c35f5c0a532eb1d08d3e
smit patel <smit.dpatel9924@gmail.com>
2026-04-26 05:07:06 +0000

    feat(app-config): add GlobalErrorHandler to application configuration

--- clients/main/src/app/app.config.ts
+++ clients/main/src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { loaderInterceptor } from './interceptors/loader/loader-interceptor';
+import { httpConfigInterceptor } from './interceptors/http-config/http-config-interceptor';
+import { GlobalErrorHandler } from './error/global-error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler,
+    },
     provideRouter(routes),
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
