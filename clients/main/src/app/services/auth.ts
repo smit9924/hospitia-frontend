9708580089e488f7a44feeec3da5a98cdadee12c
smit patel <smit.dpatel9924@gmail.com>
2026-05-01 18:53:45 +0000

    refactor(auth): streamline login method and improve session management

--- clients/main/src/app/services/auth.ts
+++ clients/main/src/app/services/auth.ts
@@ -1,9 +1,11 @@
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { apiRoutes } from '../data/api-routes';
-import { LoginApiResponse } from '../types/interfaces/auth';
-import { ApiErrorResponse } from '../types/interfaces/common';
+import { LoginApiResponseDto } from '../types/interfaces/auth';
 import { Localstorage } from './localstorage';
+import { Observable } from 'rxjs';
+import { LOCAL_STORAGE_KEYS } from '../data/localstorage-keys';
+import { LoginApiResponse } from '../types/models/auth/login-api-response';
 
 @Injectable({
   providedIn: 'root',
@@ -12,23 +14,22 @@ export class Auth {
   private http = inject(HttpClient);
   private localstorageService = inject(Localstorage);
 
-  login(
-    email: string | null | undefined,
-    password: string | null | undefined,
-    rememberMe: boolean | null | undefined,
-  ): void {
-    if (email != null && password != null && rememberMe != null) {
-      const body = new HttpParams().set('username', email).set('password', password);
+  login(email: string, password: string, _rememberMe: boolean): Observable<LoginApiResponseDto> {
+    const body = new HttpParams().set('username', email).set('password', password);
+    return this.http.post<LoginApiResponseDto>(apiRoutes.auth.login, body);
+  }
 
-      this.http.post<LoginApiResponse>(apiRoutes.auth.login, body).subscribe({
-        next: (token) => {
-          this.localstorageService.setItem(LOCAL_STORAGE_KEYS.authToken, token.access_token);
-          this.localstorageService.setItem(LOCAL_STORAGE_KEYS.refreshToken, token.refresh_token);
-        },
-        error: (error: HttpErrorResponse) => {
-          const apiError = error.error as ApiErrorResponse<null>;
-        },
-      });
-    }
+  setLoginSession(data: LoginApiResponse): void {
+    this.localstorageService.setItem(LOCAL_STORAGE_KEYS.authToken, data.accessToken);
+    this.localstorageService.setItem(
+      LOCAL_STORAGE_KEYS.authTokenExpiry,
+      data.accessTokenExpiryDate,
+    );
+    this.localstorageService.setItem(LOCAL_STORAGE_KEYS.refreshToken, data.refreshToken);
+  }
+
+  logout(): void {
+    this.localstorageService.removeItem(LOCAL_STORAGE_KEYS.authToken);
+    this.localstorageService.removeItem(LOCAL_STORAGE_KEYS.refreshToken);
   }
 }
