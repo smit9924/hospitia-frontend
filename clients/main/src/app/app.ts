073dbbe95ec2287dbf6091d3d318f72a14a34044
smit patel <smit.dpatel9924@gmail.com>
2026-05-08 17:29:10 +0000

    feat(theme): enhance theme management by initializing theme based on user preference and system settings

--- clients/main/src/app/app.ts
+++ clients/main/src/app/app.ts
@@ -2,16 +2,20 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderContainer } from './components/loaders/loader-container/loader-container';
 import { Icon } from './services/icon';
+import { Navbar } from './components/common/navbar/navbar';
+import { Footer } from './components/common/footer/footer';
+import { Theme } from './services/theme';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoaderContainer],
+  imports: [RouterOutlet, LoaderContainer, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   // Initialize icon service to register icons globally on application start
   private readonly _iconService: Icon = inject(Icon);
+  private readonly _themeService: Theme = inject(Theme);
 
   protected readonly title = signal('main');
 }
--- clients/main/src/app/services/theme.ts
+++ clients/main/src/app/services/theme.ts
@@ -17,6 +17,9 @@ export class Theme implements OnDestroy {
 
   constructor() {
     this.mediaQuery?.addEventListener('change', this.mediaQueryListener);
+
+    // Initialize theme based on stored preference or system preference
+    this.initializeTheme();
   }
 
   ngOnDestroy(): void {
@@ -32,15 +35,41 @@ export class Theme implements OnDestroy {
     this.renderer.addClass(body, themeClass);
     this.localStorageService.setItem(LOCAL_STORAGE_KEYS.preferredTheme, themeClass);
     this.themePreference.set(themeClass);
+
+    // Update active theme based on user preference
+    if(themeClass === ThemePreference.AUTO) {
+      this.activeTheme.set(this._activeTheme);
+    } else {
+      this.activeTheme.set(themeClass === ThemePreference.DARK ? ThemeType.DARK : ThemeType.LIGHT);
+    }
   }
 
   private mediaQueryListener(event: MediaQueryListEvent): void {
-    this.activeTheme.update(() => {
-      if (event.matches) {
-        return ThemeType.DARK;
-      } else {
-        return ThemeType.DARK;
-      }
-    });
+    if (this.themePreference() === ThemePreference.AUTO) {
+      // Update active theme based on system preference only if user preference is set to AUTO
+      this.activeTheme.update(() => {
+        if (event.matches) {
+          return ThemeType.DARK;
+        } else {
+          return ThemeType.LIGHT;
+        }
+      });
+    }
+  }
+
+  private initializeTheme(): void {
+    const storedTheme = this.localStorageService.getItem(
+      LOCAL_STORAGE_KEYS.preferredTheme,
+    ) as ThemePreference | null;
+
+    if (storedTheme) {
+      this.setTheme(storedTheme);
+    } else {
+      this.setTheme(ThemePreference.AUTO);
+    }
+  }
+
+  private get _activeTheme(): ThemeType {
+    return this.mediaQuery?.matches ? ThemeType.DARK : ThemeType.LIGHT;
   }
 }
