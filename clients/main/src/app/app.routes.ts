73fbec1bff95ad99e246928a3c08df961a8e5f68
smit patel <smit.dpatel9924@gmail.com>
2026-05-10 05:25:18 +0000

    feat(dashboards): add admin, customer, manager, and owner dashboard components with routing

--- clients/main/src/app/app.routes.ts
+++ clients/main/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { StyleGuide } from './components/style-guide/style-guide/style-guide';
 import { Login } from './components/auth/login/login';
 import { Signup } from './components/auth/signup/signup';
 import { ForgotPassword } from './components/auth/forgot-password/forgot-password';
+import { OwnerDashboard } from './components/dashboards/owner-dashboard/owner-dashboard';
+import { CustomerDashboard } from './components/dashboards/customer-dashboard/customer-dashboard';
+import { ManagerDashboard } from './components/dashboards/manager-dashboard/manager-dashboard';
+import { AdminDashboard } from './components/dashboards/admin-dashboard/admin-dashboard';
 
 export const routes: Routes = [
   {
@@ -21,4 +25,25 @@ export const routes: Routes = [
     path: 'forgot-password',
     component: ForgotPassword,
   },
+  {
+    path: 'dashboard',
+    children: [
+      {
+        path: 'admin',
+        component: AdminDashboard,
+      },
+      {
+        path: 'owner',
+        component: OwnerDashboard,
+      },
+      {
+        path: 'manager',
+        component: ManagerDashboard,
+      },
+      {
+        path: 'customer',
+        component: CustomerDashboard,
+      }
+    ]
+  }
 ];
---
+++ clients/main/src/app/components/dashboards/manager-dashboard/manager-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-dashboard',
+  imports: [],
+  templateUrl: './manager-dashboard.html',
+  styleUrl: './manager-dashboard.scss',
+})
+export class ManagerDashboard {
+
+}
