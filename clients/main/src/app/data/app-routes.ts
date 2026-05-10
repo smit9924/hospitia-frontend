34d4742bb96d06a61c402e1fbd6206a483311db4
smit patel <smit.dpatel9924@gmail.com>
2026-05-10 11:55:40 +0000

    feat(routes): add dashboard routes for admin, owner, manager, and customer

--- clients/main/src/app/data/app-routes.ts
+++ clients/main/src/app/data/app-routes.ts
@@ -2,4 +2,11 @@ export const APP_ROUTES = {
   login: '/login',
   signup: '/signup',
   forgotPassword: '/forgot-password',
+
+  // Dashboards
+  adminDashboard: '/dashboard/admin',
+  ownerDashboard: '/dashboard/owner',
+  managerDashboard: '/dashboard/manager',
+  customerDashboard: '/dashboard/customer',
+  // *** End Dashboards ***
 };
