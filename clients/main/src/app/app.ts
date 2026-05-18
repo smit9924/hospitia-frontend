0b90c9c44159050ab2b5ef546d44a83affabb951
smit patel <smit.dpatel9924@gmail.com>
2026-05-18 05:45:23 +0000

    refactor: replace navbar and footer visibility services with signals to prevent state persistence across route navigation

--- clients/main/src/app/app.ts
+++ clients/main/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { LoaderContainer } from './components/loaders/loader-container/loader-container';
 import { Icon } from './services/icon';
 import { Navbar } from './components/common/navbar/navbar';
-import { Navbar as NavbarService } from './services/navbar';
-import { Footer as FooterService } from './services/footer';
 import { Footer } from './components/common/footer/footer';
 import { Theme } from './services/theme';
+import { distinctUntilChanged, filter, map, shareReplay, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +13,52 @@ import { Theme } from './services/theme';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit, OnDestroy {
   // Initialize icon service to register icons globally on application start
   private readonly _iconService: Icon = inject(Icon);
   private readonly _themeService: Theme = inject(Theme);
-  readonly navbarService: NavbarService = inject(NavbarService);
-  readonly footerService: FooterService = inject(FooterService);
+  private router: Router = inject(Router);
+  private subscriptionsToBeDisposed: Subscription[] = [];
+  private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  protected showNavbar = signal(false);
+  protected showFooter = signal(false);
 
   protected readonly title = signal('main');
+
+  ngOnInit(): void {
+    const routerEvents$ = this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route = this.activatedRoute;
+
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+
+          return {
+            showNavbar: route.snapshot.data['showNavbar'] ?? false,
+            showFooter: route.snapshot.data['showFooter'] ?? false,
+          };
+        }),
+        distinctUntilChanged((prev, curr) => {
+          return prev.showNavbar === curr.showNavbar && prev.showFooter === curr.showFooter;
+        }),
+        shareReplay(1),
+      )
+      .subscribe((data) => {
+        this.showNavbar.set(data.showNavbar);
+        this.showFooter.set(data.showFooter);
+      });
+    this.subscriptionsToBeDisposed.push(routerEvents$);
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions to prevent memory leaks
+    if (this.subscriptionsToBeDisposed !== null || this.subscriptionsToBeDisposed !== undefined) {
+      for (const subscription of this.subscriptionsToBeDisposed) {
+        subscription.unsubscribe();
+      }
+    }
+  }
 }
