68507daf0afae202c48cc4ed1db4e0ea7db9effc
smit patel <smit.dpatel9924@gmail.com>
2026-05-19 03:11:26 +0000

    feat(profile): add default route redirection, fetch profile data for authenticated users when missing, and make profile fetching synchronous using RxJS

--- clients/main/src/app/services/profile.ts
+++ clients/main/src/app/services/profile.ts
@@ -1,10 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { UserProfileDto } from '../types/interfaces/users';
 import { UserProfileModel } from '../types/models/users/user-profile-model';
 import { ApiErrorResponse } from '../types/interfaces/common';
 import { ErrorCodes } from '../types/enums/error-codes';
 import { apiRoutes } from '../data/api-routes';
+import { Router } from '@angular/router';
+import { APP_ROUTES } from '../data/app-routes';
+import { UserType } from '../types/enums/auth';
+import { catchError, firstValueFrom, map, Observable, tap, throwError } from 'rxjs';
+import { Auth } from './auth';
 
 @Injectable({
   providedIn: 'root',
@@ -12,28 +17,73 @@ import { apiRoutes } from '../data/api-routes';
 export class Profile {
   private http = inject(HttpClient);
   private userProfile: UserProfileModel | null = null;
+  private router: Router = inject(Router);
+  private authService: Auth = inject(Auth);
 
-  async fetchProfile() {
-    await this.http.get<UserProfileDto>(apiRoutes.users.profile).subscribe({
-      next: (data) => {
-        this.userProfile = new UserProfileModel(data);
-      },
-      error: (error) => {
+  fetchProfile(): Observable<UserProfileModel> {
+    return this.http.get<UserProfileDto>(apiRoutes.users.profile).pipe(
+      map((data) => new UserProfileModel(data)),
+
+      tap((profile) => {
+        this.userProfile = profile;
+      }),
+
+      catchError((error: HttpErrorResponse) => {
         const errorRes = error.error as ApiErrorResponse<unknown>;
-        if (errorRes.errorCode === ErrorCodes.USER_NOT_FOUND) {
-          console.error('User not found');
-        } else {
-          throw error;
+
+        if (errorRes?.errorCode === ErrorCodes.USER_NOT_FOUND) {
         }
-      },
-    });
+
+        return throwError(() => error);
+      }),
+    );
   }
 
-  get profile(): UserProfileModel | null {
+  async getProfile(): Promise<UserProfileModel | null> {
+    if (this.userProfile === null && this.authService.isLoggedIn) {
+      await firstValueFrom(this.fetchProfile());
+    }
     return this.userProfile;
   }
 
   clearProfile() {
     this.userProfile = null;
+  }
+
+  async getDefaultHomeRouteForUser(): Promise<string> {
+    const profileData = await this.getProfile();
+
+    // If theuser profile is null, the user should be logged out.
+    // Once logged out, no further action is required, and the login route will be returned by default.
+    if (profileData === null) {
+      return APP_ROUTES.login;
+    }
+
+    switch (profileData.role) {
+      case UserType.ADMIN: {
+        return APP_ROUTES.adminDashboard;
+      }
+
+      case UserType.OWNER: {
+        return APP_ROUTES.ownerDashboard;
+      }
+
+      case UserType.MANAGER: {
+        return APP_ROUTES.managerDashboard;
+      }
+
+      case UserType.CUSTOMER: {
+        return APP_ROUTES.customerDashboard;
+      }
+
+      default: {
+        return APP_ROUTES.login;
+      }
+    }
+  }
+
+  async redirectToDefaultHome(): Promise<void> {
+    const defaultRoute = await this.getDefaultHomeRouteForUser();
+    await this.router.navigateByUrl(defaultRoute);
   }
 }
