6983c4ca83e6635570a13bac53809916afca7900
smit patel <smit.dpatel9924@gmail.com>
2026-07-24 12:55:44 +0000

    feat: update environment configuration for production settings

--- clients/main/src/environments/environment.prod.ts
+++ clients/main/src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   tag: 'prod',
   version: '0.0.0',
+  authServiceBaseUrl: 'http://auth:9002/api/v1',
+  forgotPasswordResendTimeoutSeconds: 30,
 };
